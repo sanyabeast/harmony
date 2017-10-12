51b9f4c4dbd9b729966726761b4eaa73a92090d5
sanyabeast <a.gvrnsk@gmail.com>
2017-10-12 22:13:17 +0300

    debug thangz

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ define(["./Harmony"], function(Harmony){
             this.randomNum = Math.random();
             this.harmony = new Harmony();
             window.harmony = this.harmony;
+
+            harmony.run(function(data, cb){
+                setInterval(function(){
+                    cb(Math.random() * Math.random() * Math.random() * Math.random());
+                }, 1);
+            }, 
+            function(data){testElement.innerText = data})
         }
     }
 
